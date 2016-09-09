29d5dc90459fba2a036994240b52a0d3a54627a7
daxapps <daxapps777@gmail.com>
2016-09-09 08:31:52 -0500

    Section 3

--- I Am Your Father Luke/I Am Your Father Luke/ViewController.swift
+++ I Am Your Father Luke/I Am Your Father Luke/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("record button pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop redording button pressed")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("viewWillAppear called")
+    }
 }
 
